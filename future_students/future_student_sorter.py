87387468c648a0bb58983dbb3dffc9baaf379232
MrEldridge22 <David.Eldridge22@schools.sa.edu.au>
2022-10-28 14:37:43 +1030

    Student Data working Now getting percentages of each subject based on preference place

--- future_students/future_student_sorter.py
+++ future_students/future_student_sorter.py
@@ -4,10 +4,11 @@ import sqlite3
 
 conn = sqlite3.connect(':memory:')
 conn.execute('''CREATE TABLE student_selections(
-        id INTEGER AUTO INCREMENT PRIMARY KEY,
+        id INTEGER PRIMARY KEY NOT NULL,
         Firstname TEXT NOT NULL,
         Surname TEXT NOT NULL,
         Gender TEXT,
+        Year INT,
         Arts1 TEXT,
         Arts2 TEXT,
         Free1 TEXT,
@@ -32,9 +33,16 @@ res_subjects = ['7ART', '7DRA', '7DAN', '7MUS', '7DPD', '7TECH']
 # - Output a csv / excel of students with all data but mis-matched names or no student ID (aka not in EDSAS yet!)
 
 # import future students from EDSAS Export
-future_students_df = pd.read_csv('future_students/future_students_edsas_export.csv')
-# Drop Unneeded Columns, this is currently a DayMap export, need to grab EDSAS and modify accordingly
-future_students_df.drop(['Student', 'Email', 'Form', 'Group', 'House'], axis=1, inplace=True)
+future_students_df = pd.read_csv('future_students/2023 7 EDSAS Export.TXT', sep='\t', skip_blank_lines=True, skiprows=[0,1])
+
+# Split names into columns, drop columns, and rename each column
+future_students_df[['Firstname','Surname']] = future_students_df.Name.apply(lambda x: pd.Series(str(x).split(", ")))
+future_students_df.drop(['Name', 'Admin YL', 'Census YL', 'Room', 'Roll Class'], axis=1, inplace=True)
+
+future_students_df.rename(columns={'Student ID': 'StudentID',
+                                    'ED ID': 'EDID',
+                                    'Roll Class': "RollClass"}, inplace=True)
+
 # Correct text case
 future_students_df['Firstname'] = future_students_df['Firstname'].str.title()
 future_students_df['Surname'] = future_students_df['Surname'].str.title()
@@ -67,8 +75,17 @@ for column in subject_selections_df:
 # Remove Middle Names if they are present
 subject_selections_df['Firstname'] = subject_selections_df['Firstname'].str.split(" ").str[0]
 
+# Merge StudentID into Subject Selections
+subject_selections_df = pd.merge(subject_selections_df, future_students_df, on=['EDID'])
+
+# Drop from Selections and Keep EDSAS Surname and Firstname, rename Firstname and Surname columns
+subject_selections_df.drop(columns=['Surname_x', 'Firstname_x'], inplace=True)
+subject_selections_df.rename(columns={'Surname_y': 'Surname',
+                                        'Firstname_y': 'Firstname'},
+                                        inplace=True)
+
 # Reorder To Correct Order for Selections, change this as needed
-subject_selections_df = subject_selections_df[['Surname', 'Firstname', 'Gender', 'EDID', 'Arts1', 'Arts2', 'Free1', 'Free2', 'FreeRes1', 'FreeRes2', 'ArtsRes']]
+subject_selections_df = subject_selections_df[['Surname', 'Firstname', 'Gender', 'EDID', 'StudentID', 'Arts1', 'Arts2', 'Free1', 'Free2', 'FreeRes1', 'FreeRes2', 'ArtsRes']]
 
 # Rename Subjects to Subject Codes, may need to add more rules in to fully sort!
 for preference in subject_selections_df[['Arts1', 'Arts2', 'Free1', 'Free2', 'FreeRes1', 'FreeRes2', 'ArtsRes']]:
@@ -82,9 +99,11 @@ for preference in subject_selections_df[['Arts1', 'Arts2', 'Free1', 'Free2', 'Fr
 
 # Sort the Data and Remove Duplicates at the same time, insert into db table
 for student in subject_selections_df.itertuples():
-    selections = list(dict.fromkeys([student.Firstname,
+    selections = list(dict.fromkeys([student.StudentID,
+                                        student.Firstname,
                                         student.Surname,
                                         student.Gender,
+                                        '7',
                                         student.Arts1,
                                         student.Arts2,
                                         student.Free1,
@@ -93,22 +112,26 @@ for student in subject_selections_df.itertuples():
                                         student.FreeRes2,
                                         student.ArtsRes]))
         
-    selections = selections + [np.nan] * (10 - len(selections))
+    selections = selections + [np.nan] * (12 - len(selections))
     sql = '''INSERT INTO student_selections
-                (Firstname, Surname, Gender, Arts1, Arts2, Free1, Free2, FreeRes1, FreeRes2, ArtsRes)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'''
+                (id, Firstname, Surname, Gender, Year, Arts1, Arts2, Free1, Free2, FreeRes1, FreeRes2, ArtsRes)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'''
     conn.execute(sql, tuple(selections))
     conn.commit()
 
+# Get percentage of students chosen each subject for each prefernce, loop this as needed
+percentages_df = pd.DataFrame({'Subject': ['7ART', '7DRA', '7DAN', '7MUS', '7DPD', '7TECH', '7ITAO']})
+percentages_df.set_index('Subject', inplace=True)
+print(percentages_df)
+for preference in subject_selections_df[['Arts1', 'Arts2', 'Free1', 'Free2', 'FreeRes1', 'FreeRes2', 'ArtsRes']]:
+    sql = f"""SELECT {preference} as Subject, 1.0 * COUNT(*) / (SELECT COUNT(*) FROM student_selections WHERE {preference} IS NOT NULL) AS {preference}
+                FROM student_selections
+                WHERE {preference} IS NOT NULL
+                GROUP BY {preference};"""
+    temp_df = pd.DataFrame(pd.read_sql_query(sql, conn))
+    print(temp_df)
+    percentages_df.join(temp_df.set_index('Subject'))
 
-# Get percentage of students chosen each subject for each prefernce
-subj = "Free1"
-sql = f"""SELECT {subj}, 1.0 * COUNT(*) / (SELECT COUNT(*) FROM student_selections WHERE {subj} IS NOT NULL) AS Percentage
-            FROM student_selections
-            WHERE {subj} IS NOT NULL
-            GROUP BY {subj};"""
-print(pd.read_sql(sql, conn))
-# Students who haven't completed, orr fully completed selections into separate dataframe for follow up
-missing_selections_df = subject_selections_df[subject_selections_df.isna().any(axis=1)]
-missing_selections_df.to_csv('future_students/missing_selections.csv')
-
+print(percentages_df)
+# Export all data to csv for import into Student Options
+pd.read_sql('SELECT * FROM student_selections', conn).to_csv('future_students/student_options_import.csv')
