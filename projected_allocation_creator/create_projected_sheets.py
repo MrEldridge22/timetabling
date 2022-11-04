12918bb5ab4b72d13a78f3b7a5c8725f1195ec7d
MrEldridge22 <David.Eldridge22@schools.sa.edu.au>
2022-11-04 16:05:06 +1030

    Checkpoint for new idea

--- projected_allocation_creator/create_projected_sheets.py
+++ projected_allocation_creator/create_projected_sheets.py
@@ -66,9 +66,9 @@ choice_lines_yr08_dict = {
                             7:  "S2 Line 2 T3",
                             8:  "S2 Line 4 T3",
                             9:  "S2 Line 6 T3",
-                            10: "S3 Line 2 T4",
-                            11: "S4 Line 4 T4",
-                            12: "S5 Line 6 T4"
+                            10: "S2 Line 2 T4",
+                            11: "S2 Line 4 T4",
+                            12: "S2 Line 6 T4"
                             }
 
 choice_lines_yr07_dict = { 
@@ -81,9 +81,9 @@ choice_lines_yr07_dict = {
                             7:  "S2 Line 1 T3",
                             8:  "S2 Line 4 T3",
                             9:  "S2 Line 5 T3",
-                            10: "S3 Line 1 T4",
-                            11: "S4 Line 4 T4",
-                            12: "S5 Line 5 T4"
+                            10: "S2 Line 1 T4",
+                            11: "S2 Line 4 T4",
+                            12: "S2 Line 5 T4"
                             }
 
 # Subjects by Faculty
@@ -91,7 +91,7 @@ faculty_subjects_dict = {   'English':
                                         ['07 P Literacy',
                                             '07 Literacy (ATSI Focus)',
                                             '08 Literacy',
-                                            '09 Literacy P',
+                                            '09 P Literacy',
                                             '09 Literacy',
                                             '10 Creative Writing',
                                             '11 English 1',
@@ -120,7 +120,7 @@ faculty_subjects_dict = {   'English':
                                             '11 Philosophy',
                                             '11 Society & Culture',
                                             '11 Tourism',
-                                            '12 Cultural Explorations'
+                                            '12 Cultural Explorations',
                                             '12 Politics Power & People',
                                             '12 Society & Culture',
                                             '12 Society & Culture (Modified)'],
@@ -128,7 +128,7 @@ faculty_subjects_dict = {   'English':
                             'RP':           
                                         ['12 Research Project A (Yr 11s)',
                                             '12 Research Project A',
-                                            '12 Research Project A (Modified)'],
+                                            '12 Research Project (Modified)'],
 
                             'Language':     
                                         ['07 Italian',
@@ -193,8 +193,8 @@ faculty_subjects_dict = {   'English':
                                             '11 Health & Wellbeing B',
                                             '11 Positive Education',
                                             '11 Outdoor Education A',
-                                            '11 Outdoor Education B'
-                                            '11 Physical Education A'
+                                            '11 Outdoor Education B',
+                                            '11 Physical Education A',
                                             '11 Physical Education B',
                                             '12 Child Studies',
                                             '12 Power Cup (Modified)',
@@ -251,15 +251,15 @@ faculty_subjects_dict = {   'English':
                                             '09 Dance',
                                             '09 Drama',
                                             '09 Media Arts A - Film & Cinematography',
-                                            '09 Media Arts B - Animation'
+                                            '09 Media Arts B - Animation',
                                             '09 Music',
                                             '09 Visual Arts',
                                             '10 Dance A',
                                             '10 Dance B',
                                             '10 Drama A',
                                             '10 Drama B',
-                                            '10 Media Arts A - Film & Cinematography',
-                                            '10 Media Arts B - Animation',
+                                            '10 Media Arts B - Film & Cinematography',
+                                            '10 Media Arts A - Animation',
                                             '10 Music A',
                                             '10 Music B',
                                             '10 Visual Arts A',
@@ -296,18 +296,15 @@ faculty_subjects_dict = {   'English':
                                             '11 Robotic & Electronic Systems B',
                                             '12 Construction Technology',
                                             '12 Material Solutions Metal (Modified)',
-                                            '12 Material Solutions Wood (Modified)'
+                                            '12 Material Solutions Wood (Modified)',
                                             '12 Material Solutions - Metalwork',
-                                            '12 Material Solutions - Furniture Construction'
+                                            '12 Material Solutions - Furniture Construction',
                                             '12 Robotic & Electronic Systems'],
                             'PLP':
                                         ['11 Personal Learning Plan',
                                             '12 Workplace Practices',
                                             '12 Workplace Practices (Modified)']
                                     }
-
-faculty_subjects_df = pd.DataFrame.from_dict(faculty_subjects_dict, orient='index').T
-# print(faculty_subjects_df)
 
 # Create tempory database in memory.
 conn = sqlite3.connect(':memory:')
@@ -427,7 +424,9 @@ with open('V:\\Timetabler\\Current Timetable\\2023\\V10 Files\\2023 Year 8 Stude
 ### YEAR 11 & 12 ###
 # Extract Line Info
 lines_df = pd.json_normalize(yrSS_sfx, record_path=['Lines'])
-lines_df.drop(['Code', 'LineTagID', 'LineNo', 'Classes'], inplace=True, axis=1)
+for col in lines_df.columns:
+    if col not in ['LineID', 'Name', 'Subgrid']:
+        lines_df.drop([col], inplace=True, axis=1)
 lines_df.to_sql('yrSS_lines', conn, if_exists='append', index=False)
 
 # Extract Subject Info
@@ -463,7 +462,9 @@ yrSS_df = pd.read_sql(query, conn)
 ### YEAR 10 ###
 # Extract Line Info
 lines_df = pd.json_normalize(yr10_sfx, record_path=['Lines'])
-lines_df.drop(['Code', 'LineTagID', 'LineNo', 'Classes'], inplace=True, axis=1)
+for col in lines_df.columns:
+    if col not in ['LineID', 'Name', 'Subgrid']:
+        lines_df.drop([col], inplace=True, axis=1)
 lines_df.to_sql('yr10_lines', conn, if_exists='append', index=False)
 
 # Extract Subject Info
@@ -499,7 +500,9 @@ yr10_df = pd.read_sql(query, conn)
 ### YEAR 09 ###
 # Extract Line Info
 lines_df = pd.json_normalize(yr09_sfx, record_path=['Lines'])
-lines_df.drop(['Code', 'LineTagID', 'LineNo', 'Classes'], inplace=True, axis=1)
+for col in lines_df.columns:
+    if col not in ['LineID', 'Name', 'Subgrid']:
+        lines_df.drop([col], inplace=True, axis=1)
 lines_df.to_sql('yr09_lines', conn, if_exists='append', index=False)
 
 # Extract Subject Info
@@ -586,12 +589,73 @@ def faculty_name_return(X):
             return key
     return "Key doesnt exist"
 
-
+# Put Faculties onto Dataframes
 yrSS_df['faculty'] = yrSS_df.apply(lambda row: faculty_name_return(row.subject), axis=1)
 yr10_df['faculty'] = yr10_df.apply(lambda row: faculty_name_return(row.subject), axis=1)
 yr09_df['faculty'] = yr09_df.apply(lambda row: faculty_name_return(row.subject), axis=1)
 yr08_df['faculty'] = yr08_df.apply(lambda row: faculty_name_return(row.subject), axis=1)
 
-print(yr10_df)
+full_subjects_df = pd.concat([yrSS_df, yr10_df, yr09_df, yr08_df], ignore_index=True)
+full_subjects_df = full_subjects_df[full_subjects_df['faculty'] != "Key doesnt exist"]
+
+# Remove HPE and Italian Terms for 7's and 8's as they are semester based subjects
+def remove_terms_hpe(r):
+    if r.subject in ["07 Health & Physical Education", '07 Italian', '07 Italian (Optional)', '08 Health & Physical Education', '08 Italian', '08 Italian (Optional)']:
+        if 'T1' in r.line or 'T2' in r.line or 'T3' in r.line or 'T4' in r.line:
+            r.line = r.line[:-3]
+    return r
+full_subjects_df = full_subjects_df.apply(lambda row: remove_terms_hpe(row), axis=1)
+
+# Split into Semesters and strip off Semester Codes
+semester_1_df = full_subjects_df[full_subjects_df.line.str.contains('S1')]
+semester_1_df['line'] = semester_1_df['line'].str.replace("S1 ", "")
+semester_2_df = full_subjects_df[full_subjects_df.line.str.contains('S2')]
+semester_2_df['line'] = semester_2_df['line'].str.replace("S2 ", "")
+
 ### OUTPUTS
+workbook = xlsxwriter.Workbook('projected_allocation_creator\Subject Allocations.xlsx')
+semester_1_sheet = workbook.add_worksheet(name="Semester 1")
+
+# Fucntion to creating the excel sheet
+def sheet_writer(semester_df, semester_sheet):
+    semester_sheet.write("A2", "Lines")
+    semester_sheet.merge_range('B2:C2', "Line 1", workbook.add_format({'font_name': 'Arial', 'font_size': 8, 'bold': True, 'align': "center", 'bg_color': "#FFC000", 'bottom': True, 'right': True}))
+    semester_sheet.merge_range('D2:E2', "Line 2", workbook.add_format({'font_name': 'Arial', 'font_size': 8, 'bold': True, 'align': "center", 'bg_color': "#8064A2", 'bottom': True, 'right': True}))
+    semester_sheet.merge_range('F2:G2', "Line 3", workbook.add_format({'font_name': 'Arial', 'font_size': 8, 'bold': True, 'align': "center", 'bg_color': "#FF66CC", 'bottom': True, 'right': True}))
+    semester_sheet.merge_range('H2:I2', "Line 4", workbook.add_format({'font_name': 'Arial', 'font_size': 8, 'bold': True, 'align': "center", 'bg_color': "#FF0000", 'bottom': True, 'right': True}))
+    semester_sheet.merge_range('J2:K2', "Line 5", workbook.add_format({'font_name': 'Arial', 'font_size': 8, 'bold': True, 'align': "center", 'bg_color': "#00B050", 'bottom': True, 'right': True}))
+    semester_sheet.merge_range('L2:M2', "Line 6", workbook.add_format({'font_name': 'Arial', 'font_size': 8, 'bold': True, 'align': "center", 'bg_color': "#FFFF00", 'bottom': True, 'right': True}))
+    semester_sheet.merge_range('N2:O2', "Line 7", workbook.add_format({'font_name': 'Arial', 'font_size': 8, 'bold': True, 'align': "center", 'bg_color': "#00B0F0", 'bottom': True, 'right': True}))
+
+    line_counter = 0
+
+    for faculty in faculty_subjects_dict.keys():
+        temp_df = semester_df[semester_df['faculty'] == faculty]
+        # Line 1
+        line_1_df = temp_df[temp_df['line'] == 'Line 1'].drop(['line', 'faculty'], axis=1)
+
+        # Line 2
+        line_2_df = temp_df[temp_df['line'] == 'Line 2'].drop(['line', 'faculty'], axis=1)
+
+        # Line 3
+        line_3_df = temp_df[temp_df['line'] == 'Line 3'].drop(['line', 'faculty'], axis=1)
+
+        # Line 4
+        line_4_df = temp_df[temp_df['line'] == 'Line 4'].drop(['line', 'faculty'], axis=1)
+
+        # Line 5
+        line_5_df = temp_df[temp_df['line'] == 'Line 5'].drop(['line', 'faculty'], axis=1)
+
+        # Line 6
+        line_6_df = temp_df[temp_df['line'] == 'Line 6'].drop(['line', 'faculty'], axis=1)
+
+        # Line 7
+        line_7_df = temp_df[temp_df['line'] == 'Line 7'].drop(['line', 'faculty'], axis=1)
+        
+
+
+
+
 # Semester 1 and Semester 2 Staffing Sheets
+
+sheet_writer(semester_1_df, semester_1_sheet)
