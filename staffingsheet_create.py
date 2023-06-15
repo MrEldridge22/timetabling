feb1b16ef28baa0d51c596b99e655a8777b2cebb
David Eldridge <david.eldridge1987@gmail.com>
2023-06-15 22:28:56 +0930

    Code Optimisations Put paths to files in as variables Removed repeated code in staffingsheet_create.py

--- staffingsheet_create.py
+++ staffingsheet_create.py
@@ -9,164 +9,122 @@ import pandas as pd
 """ 
 TODO:
 - UI to load tdf files, line strucutre and core groups, website?
-- Modify for Term based subjects to only count 1
+- Optimise semester and location if statements
 """
 # Debugging
 pd.get_option('display.max_columns', None)
 pd.set_option('display.max_rows', 200)
 
-# Set file location here
-school = True
-home = False
+### VARIABLES & SWITCHES ###
+# Set file location
+school = False
+home = True
 
-# Set which semester to create sheet for here
+# Year Creation
+year = 2023
+# Set which semester to create sheet for
 semester = 2
+
+# File Paths
+# School
+main_path_school    = f"V:\\Timetabler\\Current Timetable\\{year}\\V10 Files\\"
+school_sem1         = f"{main_path_school}TTD_{year}_S1.tfx"
+school_sem1_t2      = f"{main_path_school}TTD_{year}_S1_T2.tfx"
+school_sem2         = f"{main_path_school}TTD_{year}_S2.tfx"
+school_sem2_t4      = f"{main_path_school}TTD_{year}_S2_T4.tfx"
+# Home
+main_path_home      = f"C:\\Users\\demg\\OneDrive - Department for Education\\Documents\\Timetabling\\{year}\\V10 Files\\"
+home_sem1           = f"{main_path_home}TTD_{year}_S1.tfx"
+home_sem1_t2        = f"{main_path_home}TTD_{year}_S1_T2.tfx"
+home_sem2           = f"{main_path_home}TTD_{year}_S2.tfx"
+home_sem2_t4        = f"{main_path_home}TTD_{year}_S2_T4.tfx"
 
 # Database setup
 conn = sqlite3.connect(':memory:')
 createTables(conn)
 print("Database Created Sucessfully!")
 
-# Run Different apspects based on which application is loaded
-
+# Run program with different semesters or locations
 if semester == 1:
-    title_heading = "2023 Teaching Staff Semester 1"
+    title_heading = f"{year} Teaching Staff Semester 1"
     if school:
-        # Open the json tdx file
-        with open("V:\\Timetabler\\Current Timetable\\2023\\V10 Files\\TTD_2023_S1.tfx", "r") as read_content:
-            tfx_raw = json.load(read_content)
-        
-        # Read In The Data!
-        read_in_v10_data(conn, tfx_raw, 2)
-
-        # Open the json tdx file
-        with open("V:\\Timetabler\\Current Timetable\\2023\\V10 Files\\TTD_2023_S1_T2.tfx", "r") as read_content:
-            tfx_raw_term = json.load(read_content)
-        
-        # Read In The Data!
-        read_in_v10_data(conn, tfx_raw_term, 2)
-
-        # Create the workbook object with filename
-        workbook = xlsxwriter.Workbook('Subject Allocations Semester 1.xlsx')
-
-        # Populate excel sheet, do not pass faculty value to get_df function to get entire staff!
-
-        create_excel_sheet(workbook, get_df(conn), sheet_name="All Staff", heading=title_heading)
-
-        # Create separate sheets for each faculty
-        for faculty in get_faculties(conn):
-            if faculty not in ["Care", "Exec", "PT"]:
-                create_excel_sheet(workbook, get_df(conn, faculty), sheet_name=faculty, heading=title_heading)
-            else:
-                pass
-
-        # Write out the workbook
-        write_workbook(workbook)
+        sem1 = f"{main_path_school}TTD_{year}_S1.tfx"
+        sem1_t2 = f"{main_path_school}TTD_{year}_S1_T2.tfx"
 
     elif home:
-        # Open the json tdx file
-        with open("C:\\Users\\deldridge\\OneDrive - Department for Education\Documents\\Timetabling\\2023\\V10 Files\\TTD_2023_S1.tfx", "r") as read_content:
-            tfx_raw = json.load(read_content)
-        
-        # Read In The Data!
-        read_in_v10_data(conn, tfx_raw, 1)
-
-        # Open the json tdx file
-        with open("C:\\Users\\deldridge\\OneDrive - Department for Education\Documents\\Timetabling\\2023\\V10 Files\\TTD_2023_S1_T2.tfx", "r") as read_content:
-            tfx_raw_term = json.load(read_content)
-        
-        # Read In The Data!
-        read_in_v10_data(conn, tfx_raw_term, 2)
-
-        # Create the workbook object with filename
-        workbook = xlsxwriter.Workbook('Subject Allocations Semester 1.xlsx')
-
-        # Populate excel sheet, do not pass faculty value to get_df function to get entire staff!
-
-        create_excel_sheet(workbook, get_df(conn), sheet_name="All Staff", heading=title_heading)
-
-        # Create separate sheets for each faculty
-        for faculty in get_faculties(conn):
-            if faculty not in ["Care", "Exec", "PT"]:
-                create_excel_sheet(workbook, get_df(conn, faculty), sheet_name=faculty, heading=title_heading)
-            else:
-                pass
-
-        # Write out the workbook
-        write_workbook(workbook)
+        sem1 = f"{main_path_home}TTD_{year}_S1.tfx"
+        sem1_t2 = f"{main_path_home}TTD_{year}_S1_T2.tfx"
 
     else:
         print("You need to set school or home to true!")
+
+    # Open the json tdx file
+    with open(sem1, "r") as read_content:
+        tfx_raw = json.load(read_content)
+    
+    # Open the json tdx file
+    with open(sem1_t2, "r") as read_content:
+        tfx_raw_term = json.load(read_content)
+    # Read In The Data!
+    read_in_v10_data(conn, tfx_raw, 1)
+    read_in_v10_data(conn, tfx_raw_term, 2)
+    # Create the workbook object with filename
+    workbook = xlsxwriter.Workbook('Subject Allocations Semester 1.xlsx')
+
+    # Populate excel sheet, do not pass faculty value to get_df function to get entire staff!
+    create_excel_sheet(workbook, get_df(conn), sheet_name="All Staff", heading=title_heading)
+
+    # Create separate sheets for each faculty
+    for faculty in get_faculties(conn):
+        if faculty not in ["Care", "Exec", "PT"]:
+            create_excel_sheet(workbook, get_df(conn, faculty), sheet_name=faculty, heading=title_heading)
+        else:
+            pass
+
+    # Write out the workbook
+    write_workbook(workbook)
 
 elif semester == 2:
-    title_heading = "2023 Teaching Staff Semester 2"
+    title_heading = f"{year} Teaching Staff Semester 2"
     if school:
         # Open the json tdx file
-        with open("V:\\Timetabler\\Current Timetable\\2023\\V10 Files\\TTD_2023_S2.tfx", "r") as read_content:
+        with open(school_sem2, "r") as read_content:
             tfx_raw = json.load(read_content)
-        
-        # Read In The Data!
-        read_in_v10_data(conn, tfx_raw, 3)
 
         # Open the json tdx file
-        with open("V:\\Timetabler\\Current Timetable\\2023\\V10 Files\\TTD_2023_S2_T4.tfx", "r") as read_content:
+        with open(school_sem2_t4, "r") as read_content:
             tfx_raw_term = json.load(read_content)
-        
-        # Read In The Data!
-        read_in_v10_data(conn, tfx_raw_term, 4)
-
-        # Create the workbook object with filename
-        workbook = xlsxwriter.Workbook('Subject Allocations Semester 2.xlsx')
-
-        # Populate excel sheet, do not pass faculty value to get_df function to get entire staff!
-
-        create_excel_sheet(workbook, get_df(conn), sheet_name="All Staff", heading=title_heading)
-
-        # Create separate sheets for each faculty
-        for faculty in get_faculties(conn):
-            if faculty not in ["Care", "Exec", "PT"]:
-                create_excel_sheet(workbook, get_df(conn, faculty), sheet_name=faculty, heading=title_heading)
-            else:
-                pass
-
-        # Write out the workbook
-        write_workbook(workbook)
 
     elif home:
         # Open the json tdx file
-        with open("C:\\Users\\demg\\OneDrive - Department for Education\Documents\\Timetabling\\2023\\V10 Files\\TTD_2023_S2.tfx", "r") as read_content:
+        with open(home_sem2, "r") as read_content:
             tfx_raw = json.load(read_content)
-        
-        # Read In The Data!
-        read_in_v10_data(conn, tfx_raw)
 
         # Open the json tdx file
-        with open("C:\\Users\\demg\\OneDrive - Department for Education\Documents\\Timetabling\\2023\\V10 Files\\TTD_2023_S2_T4.tfx", "r") as read_content:
+        with open(home_sem2_t4, "r") as read_content:
             tfx_raw_term = json.load(read_content)
-        
-        # Read In The Data!
-        read_in_v10_data(conn, tfx_raw_term)
-
-        # Create the workbook object with filename
-        workbook = xlsxwriter.Workbook('Subject Allocations Semester 2.xlsx')
-
-        # Populate excel sheet, do not pass faculty value to get_df function to get entire staff!
-
-        create_excel_sheet(workbook, get_df(conn), sheet_name="All Staff", heading=title_heading)
-
-        # Create separate sheets for each faculty
-        for faculty in get_faculties(conn):
-            if faculty not in ["Care", "Exec", "PT"]:
-                create_excel_sheet(workbook, get_df(conn, faculty), sheet_name=faculty, heading=title_heading)
-            else:
-                pass
-
-        # Write out the workbook
-        write_workbook(workbook)
 
     else:
         print("You need to set school or home to true!")
 
+    # Read In The Data!
+    read_in_v10_data(conn, tfx_raw, 3)
+    read_in_v10_data(conn, tfx_raw_term, 4)
+    # Create the workbook object with filename
+    workbook = xlsxwriter.Workbook('Subject Allocations Semester 2.xlsx')
+
+    # Populate excel sheet, do not pass faculty value to get_df function to get entire staff!
+    create_excel_sheet(workbook, get_df(conn), sheet_name="All Staff", heading=title_heading)
+
+    # Create separate sheets for each faculty
+    for faculty in get_faculties(conn):
+        if faculty not in ["Care", "Exec", "PT"]:
+            create_excel_sheet(workbook, get_df(conn, faculty), sheet_name=faculty, heading=title_heading)
+        else:
+            pass
+
+    # Write out the workbook
+    write_workbook(workbook)
 
 # Testing Area
 # print(get_df(conn))
